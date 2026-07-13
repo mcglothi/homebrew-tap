a6a85bdbdac447e2511413a0b04c70c5368cfea4
mcglothi <timmcg@gmail.com>
2026-07-13 14:40:42 -0400

    Add Portside cask

---
+++ Casks/portside.rb
@@ -0,0 +1,25 @@
+cask "portside" do
+  version "0.5.0"
+  sha256 "713aa21ba6f04f9a523732c21dcc324db8c63eca6320b17e7d0172db698f6d46"
+
+  url "https://github.com/mcglothi/portside/releases/download/v#{version}/Portside-#{version}.zip"
+  name "Portside"
+  desc "Native SSH session manager and terminal"
+  homepage "https://github.com/mcglothi/portside"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Portside.app"
+
+  zap trash: [
+    "~/Library/Application Support/Portside",
+    "~/Library/Caches/net.timmcg.portside",
+    "~/Library/Preferences/net.timmcg.portside.plist",
+  ]
+end
